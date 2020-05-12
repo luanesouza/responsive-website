1be4ef2a2da373a6c504e588728a6fb3b6e04685
luanesouza <luanesouza411@gmail.com>
2020-05-12 12:20:12 -0400

    initial commit

---
+++ backend/db/seeds.rb
@@ -0,0 +1,77 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Person.create([{
+  name: 'Ben',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-UU3HYKQJJ-87db4a683ce7-512'
+  },
+  {
+  name: 'Eric',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-UU3HYJK1U-2265b43d1627-512'
+  },
+
+  {
+  name: 'Chris',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-UTPTF0B6X-d2d9898b47a8-512'
+  },
+
+  {
+  name: 'Andrew',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-UU0TV3MPE-dd3b4a16171f-512'
+  },
+
+  {
+  name: 'Ryan',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-UU37R6WTZ-991b72867011-512'
+  },
+
+  {
+  name: 'Ben Miguel',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-URDGHDYJZ-27ffcc4eed52-512'
+  },
+
+  {
+  name: 'Ken',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-USM2G38F3-60948b05290a-512'
+  },
+
+  {
+  name: 'Kevin',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-UTNJ0UK4J-894aae287f12-512'
+  },
+
+  {
+  name: 'Mavi',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-US8T2ES69-4089885f7348-512'
+  },
+
+  {
+  name: 'Firuz',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-USB3FA614-3c6ca16894a0-512'
+  },
+
+  {
+  name: 'Stanley',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-UTR7UP9UH-f97411482f46-512'
+  },
+
+  {
+  name: 'Isaac',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-URDG74SKA-621f345075eb-512'
+  },
+
+  {
+  name: 'Samir',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-UPVTQEC3H-105a5d3881fa-512'
+  },
+
+  {
+  name: 'Abid',
+  img_url: 'https://ca.slack-edge.com/T02MD9XTF-UT32GB6DS-47ce041c3248-512'
+  },
+  ])
